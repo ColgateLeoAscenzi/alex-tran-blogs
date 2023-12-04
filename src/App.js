4e809df57ec1f9ba8f69d712cea7b3ad38dcf7dd
Leonardo Ascenzi <lascenzi@colgate.edu>
2023-12-04 12:47:47 -0500

    added a quick proof of concept page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Alex's Blogs
-        </a>
-      </header>
+        <iframe
+            id="moduleStyle"
+            src="./pages/M8_CorrelationRegression.html"
+            title="M8Correlation Page"
+        ></iframe>
     </div>
   );
 }
